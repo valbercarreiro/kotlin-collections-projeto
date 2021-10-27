3401d1c49661c8fdd16fd619d1e7922856036a13
Valber Carreiro <valber.carreiro@m4u.com.br>
2021-10-27 16:24:04 -0300

    collections

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -2,4 +2,33 @@ package br.com.alura
 
 fun main() {
 
+    val banco = BancoDeNomes()
+    val nomesSalvos = banco.nomes
+
+    banco.salva("Alex")
+    println(nomesSalvos)
+    println(BancoDeNomes().nomes)
+
+}
+
+class BancoDeNomes {
+
+    val nomes: Collection<String> get() = dados.toList()
+
+    fun salva(nome: String) {
+        dados.add(nome)
+    }
+
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+}
+
+fun testaColecao() {
+    val nomes: MutableList<String> = mutableListOf("Alex", "Fran", "Gui", "Maria", "Ana")
+
+    nomes.add("Paulo")
+    nomes.remove("Alex")
+
+    println(nomes)
 }
