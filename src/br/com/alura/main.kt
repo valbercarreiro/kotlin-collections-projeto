5dc3160c19d8d37c269eb9d88b9074682ebcb550
Valber Carreiro <valber.carreiro@m4u.com.br>
2021-10-28 15:43:23 -0300

    trabalhando com set

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -2,33 +2,28 @@ package br.com.alura
 
 fun main() {
 
-    val banco = BancoDeNomes()
-    val nomesSalvos = banco.nomes
+    val assistiramCursoAndroid: Set<String> = setOf<String>("Alex", "Fran", "Gui", "Maria")
+    val assistiramcursoKotlin: Set<String> = setOf<String>("Alex", "Paulo", "Maria")
+    val assistiramAmbos: MutableSet<String> = mutableSetOf()
+    assistiramAmbos.addAll(assistiramCursoAndroid)
+    assistiramAmbos.addAll(assistiramcursoKotlin)
 
-    banco.salva("Alex")
-    println(nomesSalvos)
-    println(BancoDeNomes().nomes)
+    println(assistiramAmbos.distinct())
+    println()
 
-}
+    println(assistiramcursoKotlin - assistiramCursoAndroid)
+    println(assistiramcursoKotlin subtract assistiramCursoAndroid)
+    println()
 
-class BancoDeNomes {
+    println(assistiramcursoKotlin + assistiramCursoAndroid)
+    println(assistiramcursoKotlin union assistiramCursoAndroid)
+    println()
 
-    val nomes: Collection<String> get() = dados.toList()
+    println(assistiramcursoKotlin intersect assistiramCursoAndroid)
 
-    fun salva(nome: String) {
-        dados.add(nome)
-    }
+    val assistiramList = assistiramAmbos.toMutableList()
+    assistiramList.add("Alex")
+    println(assistiramList)
+    println(assistiramList.toSet())
 
-    companion object {
-        private val dados = mutableListOf<String>()
-    }
-}
-
-fun testaColecao() {
-    val nomes: MutableList<String> = mutableListOf("Alex", "Fran", "Gui", "Maria", "Ana")
-
-    nomes.add("Paulo")
-    nomes.remove("Alex")
-
-    println(nomes)
 }
