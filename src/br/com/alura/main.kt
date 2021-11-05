661350d9f2e10096ec053fe5d59828544999ad82
Valber Carreiro <valber.carreiro@m4u.com.br>
2021-11-05 10:29:14 -0300

    agrupando valores em colecoes

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -2,63 +2,45 @@ package br.com.alura
 
 fun main() {
 
-    val pedidos = mutableMapOf(Pair(1, 20.0), Pair(2, 34.0), 3 to 50.0, 4 to 110.0, 5 to 150.0, 6 to 80.0)
+    val pedidos = listOf(
+            Pedido(1, 20.0),
+            Pedido(2, 60.0),
+            Pedido(3, 30.0),
+            Pedido(4, 70.0)
+    )
 
-//    val valorPedido = pedidos.getValue(5)
-//    println(valorPedido)
+    println(pedidos)
 
-    val mensagem = pedidos.getOrElse(0) { 0.0 }
-    println(mensagem)
-
-    println(pedidos.getOrDefault(1, -1.0))
-    println(pedidos.getOrDefault(0, -1.0))
-
-    println(pedidos.keys)
-    for(numeroDePedido in pedidos.keys) {
-        println("Pedido $numeroDePedido")
+    val pedidosMapeados: Map<Int, Pedido> = pedidos.associate { pedido: Pedido ->
+        Pair(pedido.numero, pedido)
     }
+    println(pedidosMapeados)
 
-    println(pedidos.values)
-    for (valor in pedidos.values) {
-        println("Valor do pedido $valor")
+    val pedidosMapeados2: Map<Int, Pedido> = pedidos.associateBy { pedido ->
+        pedido.numero
     }
+    println(pedidosMapeados2)
 
-    val pedidosFiltrados = pedidos.filter { (numero, valor) ->
-                                numero % 2 == 0 && valor > 50.0
-                            }
-    println(pedidosFiltrados)
+    val pedidosFreteGratis: Map<Pedido, Boolean> = pedidos.associateWith { pedido ->
+        pedido.valor > 50.0
+    }
+    println(pedidosFreteGratis)
 
-    val pedidosAcimade70 = pedidos.filterValues { valor ->
-                    valor > 70.0
-                }
-    println(pedidosAcimade70)
+//    val mapa: Map<Boolean, Pedido> = pedidos.associateBy { pedido ->
+//        pedido.valor > 50.0
+//    }
+//    println(mapa)
 
-    val pedidosPares = pedidos.filterKeys { numero ->
-                    numero % 2 == 0
-                }
-    println(pedidosPares)
+    val pedidosFreteGratisAgrupados: Map<Boolean, List<Pedido>> = pedidos.groupBy { pedido ->
+        pedido.valor > 50.0
+    }
+    println(pedidosFreteGratisAgrupados)
 
-    println(pedidos + Pair(7, 90.0))
-    println(pedidos)
-
-    println(pedidos - 6)
-    println(pedidos)
-
-    println(pedidos - listOf(6, 5))
-    println(pedidos)
-
-    pedidos.putAll(setOf(Pair(7, 90.0), Pair(8, 92.0)))
-    println(pedidos)
-
-    pedidos += setOf(Pair(9, 20.0), Pair(10, 31.0))
-    println(pedidos)
-
-    pedidos.keys.remove(1)
-    println(pedidos)
-
-    pedidos.values.remove(50.0)
-    println(pedidos)
-
-    pedidos -= 6
-    println(pedidos)
+    val nomes: List<String> = listOf("Alex", "Fran", "Gui", "Ana", "Paulo", "Maria", "Mario", "João")
+    val agenda: Map<Char, List<String>> = nomes.groupBy { nome ->
+        nome.first()
+    }
+    println(agenda)
 }
+
+class Pedido(val numero: Int, val valor: Double)
--- src/br/com/alura/testaMap.kt
+++ src/br/com/alura/testaMap.kt
@@ -49,3 +49,65 @@ fun testaMap() {
     pedidosMutaveis.remove(3, 50.0)
     println(pedidosMutaveis)
 }
+
+fun testaComportamentosMap() {
+    val pedidos = mutableMapOf(Pair(1, 20.0), Pair(2, 34.0), 3 to 50.0, 4 to 110.0, 5 to 150.0, 6 to 80.0)
+
+//    val valorPedido = pedidos.getValue(5)
+//    println(valorPedido)
+
+    val mensagem = pedidos.getOrElse(0) { 0.0 }
+    println(mensagem)
+
+    println(pedidos.getOrDefault(1, -1.0))
+    println(pedidos.getOrDefault(0, -1.0))
+
+    println(pedidos.keys)
+    for (numeroDePedido in pedidos.keys) {
+        println("Pedido $numeroDePedido")
+    }
+
+    println(pedidos.values)
+    for (valor in pedidos.values) {
+        println("Valor do pedido $valor")
+    }
+
+    val pedidosFiltrados = pedidos.filter { (numero, valor) ->
+        numero % 2 == 0 && valor > 50.0
+    }
+    println(pedidosFiltrados)
+
+    val pedidosAcimade70 = pedidos.filterValues { valor ->
+        valor > 70.0
+    }
+    println(pedidosAcimade70)
+
+    val pedidosPares = pedidos.filterKeys { numero ->
+        numero % 2 == 0
+    }
+    println(pedidosPares)
+
+    println(pedidos + Pair(7, 90.0))
+    println(pedidos)
+
+    println(pedidos - 6)
+    println(pedidos)
+
+    println(pedidos - listOf(6, 5))
+    println(pedidos)
+
+    pedidos.putAll(setOf(Pair(7, 90.0), Pair(8, 92.0)))
+    println(pedidos)
+
+    pedidos += setOf(Pair(9, 20.0), Pair(10, 31.0))
+    println(pedidos)
+
+    pedidos.keys.remove(1)
+    println(pedidos)
+
+    pedidos.values.remove(50.0)
+    println(pedidos)
+
+    pedidos -= 6
+    println(pedidos)
+}
